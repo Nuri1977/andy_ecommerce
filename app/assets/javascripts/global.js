1c18fe8528691aab3febf4404b6535c276e54844
nuri1977 <nurilacka@gmail.com>
2020-05-10 11:52:42 +0200

    Javascript autofade for notifivations

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,12 @@
+  
+document.addEventListener("turbolinks:load", function() {
+
+  var notification = document.querySelector('.global-notification');
+
+  if(notification) {
+    window.setTimeout(function() {
+      notification.style.display = "none";
+    }, 4000);
+  }
+
+});
